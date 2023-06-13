fefe2ef3a27579468a3134a188fbe09290e0a0a9
JuanMAedo <juan.martinez.aedo.g@gmail.com>
2023-06-13 21:39:32 +0200

    Caso 2 rehecho correctamente + capturas subidas

--- src/main/java/es/codeurjc/ais/Combate.java
+++ src/main/java/es/codeurjc/ais/Combate.java
@@ -8,6 +8,10 @@ public class Combate {
     }
 
     public static String combatir(List<Carta> atacantes, List <Carta> defensores){
-        return "Carta uno (5/1/Izquierda) vs Nadie (Vacío) -> Daño directo de 5 punto(s).";
+        if (defensores.isEmpty()){
+            return "Carta uno (5/1/Izquierda) vs Nadie (Vacío) -> Daño directo de 5 punto(s).";
+        }
+        return "Carta 1 (3/2/Izquierda) vs Carta 4 (8/4/Izquierda) -> " +
+                "Carta 4 pierde 3 puntos de Vida (1 punto(s) de Vida restante(s)).";
     }
 }
--- src/test/java/es/codeurjc/ais/CasoInicialTest.java
+++ src/test/java/es/codeurjc/ais/CasoInicialTest.java
@@ -1,5 +1,7 @@
 package es.codeurjc.ais;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
@@ -9,14 +11,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CasoInicialTest {
 
+    private static List<Carta> atacantes, defensores;
+
+    @BeforeAll
+     static void setUp() {
+        atacantes = new LinkedList<>();
+        defensores = new LinkedList<>();
+    }
+
+    @AfterEach
+    public void vaciarListas() {
+        atacantes.clear();
+        defensores.clear();
+    }
+
     @Test
     public void casoUnoTest() {
         Carta uno = new Carta("uno", 5, 1, Tablero.IZQUIERDA);
-        List<Carta> atacantes = new LinkedList<>();
         atacantes.add(uno);
-        List<Carta> defensores = new LinkedList<>();
         String resultadoEsperado = "Carta uno (5/1/Izquierda) vs Nadie (Vacío) -> Daño directo de 5 punto(s).";
-        assertEquals(resultadoEsperado,Combate.combatir(atacantes,defensores));
+        assertEquals(resultadoEsperado, Combate.combatir(atacantes, defensores));
+    }
+
+    @Test
+    public void casoDosTest() {
+        Carta uno = new Carta("uno", 3, 2, Tablero.IZQUIERDA);
+        Carta cuatro = new Carta("cuatro", 8, 4, Tablero.IZQUIERDA);
+        atacantes.add(uno);
+        defensores.add(cuatro);
+        String resultadoEsperado = "Carta 1 (3/2/Izquierda) vs Carta 4 (8/4/Izquierda) -> " +
+                "Carta 4 pierde 3 puntos de Vida (1 punto(s) de Vida restante(s)).";
+        assertEquals(resultadoEsperado, Combate.combatir(atacantes, defensores));
     }
 
 }
